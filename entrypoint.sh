159bfc000b658732e3de1d44cceb7020991cdf9f
Credgroup <tecnologia@credgroup.com.br>
2026-09-22 14:45:11 -0300

    feat: add VITE_USE_MOCK configuration to Docker setup and entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,15 @@
 
 # Gera o arquivo .env a partir das variáveis do ambiente
 echo "Gerando arquivo .env a partir das variáveis do ambiente..."
-echo "VITE_ENV=$VITE_ENV" > .env
+echo "VITE_USE_MOCK=$VITE_USE_MOCK" > .env
+echo "VITE_ENV=$VITE_ENV" >> .env
 echo "VITE_IMAGE_VERSION=$VITE_IMAGE_VERSION" >> .env
 echo "VITE_URL_DOTCORE=$VITE_URL_DOTCORE" >> .env
 echo "VITE_URL_AUTH_DOTCORE=$VITE_URL_AUTH_DOTCORE" >> .env
 echo "VITE_PLATFORM=$VITE_PLATFORM" >> .env
 echo "VITE_AES_KEY=$VITE_AES_KEY" >> .env
 echo "VITE_AES_IV=$VITE_AES_IV" >> .env
-echo "VITE_AES_IV=$VITE_URL_CRM_DOTCORE" >> .env
+echo "VITE_URL_CRM_DOTCORE=$VITE_URL_CRM_DOTCORE" >> .env
 echo "VITE_THEME_FAVICON=$VITE_THEME_FAVICON" >> .env
 echo "VITE_WS_PAYMENT_URL=$VITE_WS_PAYMENT_URL" >> .env
 echo "VITE_THEME_BLOBS_KEY=$VITE_THEME_BLOBS_KEY" >> .env
